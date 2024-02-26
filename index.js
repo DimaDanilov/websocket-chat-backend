5471af2c0fa0740ada75e4bdc7aa92a361e8910e
dima danilov <dima.danilov.2012@mail.ru>
2024-02-26 12:09:41 +0300

    add: websocket logic

--- index.js
+++ index.js
@@ -8,3 +8,23 @@ const webSocketServer = new webSocket.Server(
   },
   () => console.log(`Server started on ${PORT}`)
 );
+
+webSocketServer.on("connection", function connection(socket) {
+  socket.on("message", function (message) {
+    message = JSON.parse(message);
+    switch (message.event) {
+      case "message":
+        broadcastMessage(message);
+        break;
+      case "connection":
+        broadcastMessage(message);
+        break;
+    }
+  });
+});
+
+function broadcastMessage(message) {
+  webSocketServer.clients.forEach((client) => {
+    client.send(JSON.stringify(message));
+  });
+}
